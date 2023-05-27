1fb5356019512ffd058981233e685a3c7be4a922
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-27 17:28:07 -0600

    File Setup and MUI installation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            <h1>Navigation</h1>
+            <p>This is the navigation.</p>
+        </div>
+    );
+}
+
+export default Navigation;
---
+++ src/pages/Experience/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <div>
+            <h1>Experience Page</h1>
+            <p>This is the Experience page.</p>
+        </div>
+    );
+}
+
+export default Experience;
