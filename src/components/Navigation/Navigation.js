8b8a30a3d40e2cb642af2580b96adfaef8ce988a
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-27 23:18:34 -0600

    Custom Icon and Title

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,6 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import LogoDevIcon from '@mui/icons-material/LogoDev';
 import { Link as ScrollLink } from 'react-scroll';
