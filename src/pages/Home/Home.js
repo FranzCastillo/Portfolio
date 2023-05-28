e9b7cf0dc9879dbffbb49e6fd63370dcc358e9cd
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-27 23:56:52 -0600

    Home Placeholder

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,35 +1,38 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 import './Home.scss';
 
 const Home = () => {
-    return (
-        <div className={"home"} id={"home"}>
-            <h1>Home Page</h1>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-            <p>This is the home page.</p>
-        </div>
-    );
-}
+        return (
+            <Container
+                className={"home"}
+                maxWidth="xl"
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    padding: '15px',
+                    fontFamily: 'Roboto, sans-serif',
+                }}
+            >
+                <Typography
+                    className={"title"}
+                    variant="h4"
+                    component="h1"
+                    gutterBottom>
+                    ¡Hola! Soy <span id={"name"}>Francisco Castillo.</span>👋
+                </Typography>
+                <Typography
+                    className={"title"}
+                    variant="h4"
+                    component="h1"
+                    gutterBottom>
+                    Alch, no se que poner aquí.😅
+                </Typography>
+            </Container>
+        );
+};
 
 export default Home;
