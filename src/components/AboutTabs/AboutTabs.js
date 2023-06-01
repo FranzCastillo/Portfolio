b7e3a82c1b9124349bd5f617155d0fb18d2ba45c
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-31 21:14:16 -0600

    Soft Skills Implemented

--- src/components/AboutTabs/AboutTabs.js
+++ src/components/AboutTabs/AboutTabs.js
@@ -7,6 +7,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import EducationTimeline from "./Education/EducationTimeline";
 import SkillsAccordion from "./HardSkills/SkillsAccordion/SkillsAccordion";
+import SoftSkillsDisplay from "./SoftSkills/SoftSkillsDisplay";
 
 function TabPanel(props) {
     const {children, value, index, ...other} = props;
@@ -74,6 +75,7 @@ export default function AboutTabs() {
                 <SkillsAccordion />
             </TabPanel>
             <TabPanel value={value} index={2}>
+                <SoftSkillsDisplay  />
             </TabPanel>
         </Box>
     );
