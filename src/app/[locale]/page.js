59a144d4ab6bb2c9f0b9b0063bfe22615643b567
Francisco Castillo <122114861+FranzCastillo@users.noreply.github.com>
2024-06-18 16:55:18 -0600

    Updated Hero
    Also, removed DaisyUI

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,20 +1,11 @@
-import {useTranslations} from 'next-intl';
+import HeroSection from "@/components/heroSection";
 
 export default function Home() {
-    const t = useTranslations('Hero');
-
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-                     className="max-w-sm rounded-lg shadow-2xl"/>
-                <div>
-                    <h1 className="text-5xl font-bold">{t("title")}</h1>
-                    <p className="py-6">{t("description")}</p>
-                    <button className="btn btn-primary mr-3">{t("cv-button")}</button>
-                    <button className="btn btn-secondary">{t("more-button")}</button>
-                </div>
+        <main className={"flex min-h-screen flex-col bg-[#131f24]"}>
+            <div className={"container mx-auto px-12 py-4"}>
+                <HeroSection/>
             </div>
-        </div>
+        </main>
     );
 }
---
+++ src/components/heroSection.js
@@ -0,0 +1,64 @@
+"use client";
+import {useTranslations} from 'next-intl';
+import Image from "next/image";
+import {TypeAnimation} from "react-type-animation";
+
+
+export default function HeroSection() {
+    const t = useTranslations('Hero');
+
+    return (
+        <section>
+            <div className={"grid grid-cols-1 sm:grid-cols-12"}>
+                <div className={"col-span-7 place-self-center text-center sm:text-left"}>
+                    <h1 className={"text-white mb-4 text-4xl lg:text-6xl font-extrabold"}>
+                        <span className={"text-[#89e219]"}>{t("title")}</span><br/>
+                        <TypeAnimation
+                            sequence={[
+                                'Francisco',
+                                4000,
+                                t("roles.student"),
+                                2500,
+                                t("roles.software-engineer"),
+                                2500,
+                                t("roles.ai-enthusiast"),
+                                2500,
+                                t("roles.tech-lover"),
+                                2500,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                            preRenderFirstString={true}
+                        />
+                    </h1>
+                    <p className={"text-[#A1AFAC] text-base sm:text-lg lg:text-xl mb-6"}>
+                        {t.rich("description",{
+                            age: () => new Date().getFullYear() - 2002,
+                        })}
+                    </p>
+                    <div>
+                        <button className={"px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-[#89e219] hover:bg-[#58cc02]"}>
+                            {t("more-button")}
+                        </button>
+                        <button
+                            className={"px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-[#1f333b] text-[#A1AFAC] border border-[#A1AFAC] mt-3"}>
+                            {t("cv-button")}
+                        </button>
+                    </div>
+                </div>
+                <div className={"col-span-5 place-self-center mt-4 lg:mt-0"}>
+                    <div className={"bg-[#1f333b] lg:w-[400px] lg:h-[400px] w-[250px] h-[250px] rounded-full relative overflow-hidden"}>
+                        <Image
+                            src={"/images/DuolingoAvatar.png"}
+                            alt={"hero image"}
+                            className={"absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 max-w-none lg:w-[650px]"}
+                            width={400}
+                            height={400}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
